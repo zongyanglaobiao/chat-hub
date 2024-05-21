895d03bcfac4a9a0f37ff90c97bbf91eb995bf35
xxl <3578144921@qq.com>
2024-05-21 14:23:22 +0800

    feat(搜索页面):增加样式

--- front-end/src/views/home/chat/search/Search.jsx
+++ front-end/src/views/home/chat/search/Search.jsx
@@ -1,28 +1,85 @@
-import {LeftCircleTwoTone} from "@ant-design/icons";
+import {SearchOutlined} from "@ant-design/icons";
 import {useLocation, useNavigate} from "react-router-dom";
-import {Button} from "antd";
+import {useEffect, useState} from "react";
+import {Button, Input, List, Select, Space} from "antd";
 
-function Search() {
+const { Search } = Input;
+
+function SearchPage() {
+    //找到来这页面的路由
     const location = useLocation();
-    //从哪里来的就从哪里回
     const navigate = useNavigate();
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        setList([{id:1,name:'xxxx'},{id:2,name:'xxxx'}])
+    }, []);
+
+    const options = [
+        {label: '所有', value: 'ALL'},
+        {label: '用户', value: 'USER'},
+        {label: '朋友', value: 'FRIEND'},
+        {label: '群', value: 'GROUP'},
+    ]
 
     return (
-        <div>
-            <header>
-                <LeftCircleTwoTone style={{fontSize: 50}} onClick={()=>navigate(location.state)}/>
-                <Button onClick={()=>{
-                    //发出请求
-                }}>
-                    获取search value
-                </Button>
+        <div className=' w-full text-center relative'>
+            <header className='relative top-20px'>
+               <Space>
+                   <Button
+                       shape="circle"
+                       onClick={() =>  {
+                           console.log('点击')
+                           navigate(location.state)
+                       }}
+                       icon={ <SearchOutlined /> }
+                   >
+                   </Button>
+                   <Search placeholder="input search text"
+                           onSearch={(value)=> {
+                               console.log(value)
+                           }}
+                           enterButton />
+                   <Select
+                       defaultValue="所有"
+                       options={options}
+                       style={{
+                           width: 120,
+                       }}
+                       onChange={(value, option)=>{
+                           console.log(value,option)
+                       }}/>
+               </Space>
             </header>
             <main>
-
+                <div className="overflow-auto max-h-80">
+                    <List
+                        loadMore = {(
+                            <div
+                                style={{
+                                    textAlign: 'center',
+                                    marginTop: 12,
+                                    height: 32,
+                                    lineHeight: '32px',
+                                }}
+                            >
+                                <Button onClick={()=>{
+                                    setList([...list,{id:3,name:'xxxx'}])
+                                }}>loading more</Button>
+                            </div>
+                        )}
+                        dataSource={list}
+                        renderItem={item => (
+                            <List.Item key={item.id}>
+                                {item.name}
+                            </List.Item>
+                        )}
+                    />
+                </div>
             </main>
         </div>
     )
 }
 
 
-export default Search;
+export default SearchPage;
