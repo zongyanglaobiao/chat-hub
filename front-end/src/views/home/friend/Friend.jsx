374041a2a8abc165589000396a7c7b5e5bdf6c83
xxl <3578144921@qq.com>
2024-06-14 11:37:32 +0800

    feat: 完成好友数据展示

--- front-end/src/views/home/friend/Friend.jsx
+++ front-end/src/views/home/friend/Friend.jsx
@@ -3,48 +3,63 @@ import ChatTable from "@/component/table/ChatTable.jsx";
 import {ChatList} from "@/component/list/ChatList.jsx";
 import {useSelector} from "react-redux";
 import {doQueryUserInfos} from "@/http/api/user.api.js";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
+import {DisplayNoneImageContext} from "@/views/App.jsx";
 
 const Friend = () => {
     //好友信息
     const {friendList,unprocessedList,applicationList} = useSelector(state => state.friendInfo)
     const [friendInfo, setFriendInfo] = useState([])
+    const [unprocessInfo, setUnprocessInfo] = useState([])
+    const [applicationInfo, setApplicationInfo] = useState([])
 
-    const fetchFriendInfo = async (friendIds) => {
+    const fetchFriendInfo = async (friendIds,hook) => {
         const resp = await doQueryUserInfos(friendIds)
-
-        if (resp.code === 200) {
-            return resp.data
-        }
-
-        message.error(resp.message)
-        return []
+        resp.code === 200 ?  hook(resp.data) :  message.error(resp.message)
     }
 
     useEffect(() => {
-        friendList.map(t => t.friendId).length !== 0 && setFriendInfo(fetchFriendInfo(friendList.map(t => t.friendId)))
+        friendList.map(t => t.friendId).length !== 0 && fetchFriendInfo(friendList.map(t => t.friendId),setFriendInfo)
     }, [friendList]);
 
-    // useEffect(() => {
-    //     doQueryUserInfosResp.length !== 0 && setFriendInfo(doQueryUserInfosResp.data)
-    // }, [doQueryUserInfosResp]);
+    useEffect(() => {
+        unprocessedList.length !== 0 && fetchFriendInfo(unprocessedList.map(t => t.friendId),setUnprocessInfo)
+    }, [unprocessedList]);
+
+    useEffect(() => {
+        applicationList.length !== 0 && fetchFriendInfo(applicationList.map(t => t.friendId),setApplicationInfo)
+    }, [applicationList]);
     
     // 使用items属性配置每个Tab页
     const items = [
         {
             label: '好友列表',
-            key: '1',
-            children:  <ChatFriendList data={friendInfo}/>   ,
+            key: 1,
+            children:  (
+                <ChatFriendList data={friendInfo}>
+                    <Button>删除</Button>
+                    <Button>发送信息</Button>
+                </ChatFriendList>
+            )   ,
+        },
+        {
+            label: '待处理申请列表',
+            key: 2,
+            children:  (
+                <ChatFriendList data={unprocessInfo}>
+                    <Button>同意</Button>
+                    <Button>拒绝</Button>
+                </ChatFriendList>
+            ) ,
         },
         {
             label: '申请列表',
-            key: '2',
-            children:  <ChatFriendList data={friendInfo}/>
-        },
-        {
-            label: '待处理好友申请',
-            key: '3',
-            children:  <ChatFriendList data={friendInfo}/> ,
+            key: 3,
+            children:  (
+                <ChatFriendList data={applicationInfo}>
+                    <Button>删除</Button>
+                </ChatFriendList>
+            )
         },
     ];
 
@@ -53,27 +68,31 @@ const Friend = () => {
     )
 }
 
-const ChatFriendList = ({data}) => {
+const ChatFriendList = ({data,children}) => {
+    const {openOrCloseImage} = useContext(DisplayNoneImageContext)
+    console.log(data)
     return (
         data.length !== 0
             ?
             <ChatList data={data} pageSize={6} renderItem={(item) => (
                 <List.Item>
                     <List.Item.Meta
-                        avatar={<Avatar src={item.avatar} />}
+                        avatar={<Avatar src={item.avatar}
+                                        size={45}
+                                        className=" cursor-pointer"
+                                        onClick={()=>openOrCloseImage(item.avatar)} />}
                         title={item.nickname}
                         description={item.signature}
                         className="text-overflow"
                     />
-                    <Space>
-                        <Button>删除</Button>
-                        <Button>发送信息</Button>
+                    <Space className="ml-3">
+                        {children}
                     </Space>
                 </List.Item>
             )}/>
             :
             <div>
-                暂无好友
+                暂无数据
             </div>
     )
 }
