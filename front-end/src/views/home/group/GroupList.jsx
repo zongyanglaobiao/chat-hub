81403725c3cd7b8b076635f5a3f73a7e89ca47d9
xxl <3578144921@qq.com>
2024-06-21 22:57:45 +0800

    feat: 群的渲染完成

--- front-end/src/views/home/group/GroupList.jsx
+++ front-end/src/views/home/group/GroupList.jsx
@@ -1,48 +1,72 @@
-import  { useState } from 'react';
+import {useEffect, useState} from 'react';
 import { List, Avatar, Typography } from 'antd';
 import {useSelector} from "react-redux";
+import {getRandomId, isNullOrUndefined} from "@/lib/toolkit/util.js";
+import {useFetch} from "@/hook/useFetch.jsx";
+import {doQueryUserInfos} from "@/http/api/user.api.js";
 
 const { Text, Title } = Typography;
 
 const ChatGroupList = () => {
-    const [selectedGroup, setSelectedGroup] = useState(null);
-    useSelector(state => state.group.groups)
+    let groupInfo = useSelector(state => state.groupInfo)
+    const [doQueryUserInfosResp,doQueryUserInfosProxy] = useFetch(doQueryUserInfos)
+    const [groupMemberInfo, setGroupMemberInfo] = useState([])
+    const [group, setGroup] = useState(null)
 
+    useEffect(() => {
+        !isNullOrUndefined(doQueryUserInfosResp) && setGroupMemberInfo(doQueryUserInfosResp?.data || [])
+    }, [doQueryUserInfosResp]);
+
+    const queryGroupMember = (group) => {
+        const memberIds = group?.members.map(t => t.userId)
+        // 保存选中的群组
+        setGroup(group)
+        // 查询群成员信息
+        memberIds.length > 0 && doQueryUserInfosProxy(memberIds)
+    }
+
+    useEffect(() => {
+        console.log('groupMemberInfo',groupMemberInfo)
+    });
 
     return (
-        <div className="flex w-full">
-            <div className="w-1/3 overflow-y-auto p-4 border-r">
+        <div className="flex w-full h-full">
+            <div className="w-1/4 overflow-y-auto  remove-the-scroll">
                 <List
+                    bordered={true}
                     itemLayout="horizontal"
-                    dataSource={groups}
+                    dataSource={groupInfo}
                     renderItem={group => (
-                        <List.Item key={group.id} onClick={() => setSelectedGroup(group)}>
+                        <List.Item
+                            key={getRandomId()}
+                            onClick={() => queryGroupMember(group)}>
                             <List.Item.Meta
                                 className=' cursor-pointer'
-                                avatar={<Avatar size={"large"} src={group.avatar} shape={"square"} />}
-                                title={<Text>{group.name}</Text>}
+                                avatar={<Avatar size={60} src={group.avatar} shape={"square"} />}
+                                title={<Text>{group.groupName}</Text>}
                             />
                         </List.Item>
                     )}
                 />
             </div>
-            <div className="w-2/3 p-4">
-                {selectedGroup ? (
+            <div className='ml-4px w-1px h-full bg-gray'></div>
+            <div className="w-3/4 p-4">
+                {group ? (
                     <>
                         <div className="mb-4">
-                            <Avatar size={74} src={selectedGroup.avatar} shape={"square"}/>
-                            <Title level={4} className="mt-2">{selectedGroup.name}</Title>
+                            <Avatar size={74} src={group.avatar} shape={"square"}/>
+                            <Title level={4} className="mt-2">{group.name}</Title>
                         </div>
                         <Title level={5}>成员列表</Title>
                         <div className='h-320px overflow-y-auto remove-the-scroll'>
                             <List
                                 itemLayout="horizontal"
-                                dataSource={selectedGroup.members}
+                                dataSource={groupMemberInfo}
                                 renderItem={member => (
-                                    <List.Item key={member.id}>
+                                    <List.Item key={getRandomId()}>
                                         <List.Item.Meta
                                             avatar={<Avatar src={member.avatar} />}
-                                            title={<Text>{member.name}</Text>}
+                                            title={<Text>{member.nickname}</Text>}
                                         />
                                     </List.Item>
                                 )}
@@ -50,7 +74,7 @@ const ChatGroupList = () => {
                         </div>
                     </>
                 ) : (
-                    <Title   className='w-full h-full flex justify-center items-center'>请选择一个群</Title>
+                    <Title  className='w-full h-full flex justify-center items-center'>请选择一个群</Title>
                 )}
             </div>
         </div>
