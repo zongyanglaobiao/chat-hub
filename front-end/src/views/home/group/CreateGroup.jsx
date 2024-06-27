ff197b913f2b372c1c8c3f488f7947b93f931a1f
xxl <3578144921@qq.com>
2024-06-27 09:33:12 +0800

    fix: 修改样式

--- front-end/src/views/home/group/CreateGroup.jsx
+++ front-end/src/views/home/group/CreateGroup.jsx
@@ -1,6 +1,6 @@
 import {useLocation, useNavigate} from "react-router-dom";
 import {memo, useEffect} from "react";
-import {Avatar, Button, message, Upload} from "antd";
+import {Avatar, Button, Flex, Input, message, Upload} from "antd";
 import {UploadOutlined} from "@ant-design/icons";
 import {getUploadUrl} from "@/http/api/file.api.js";
 import {getToken} from "@/http/http.request.js";
@@ -20,24 +20,30 @@ const CreateGroup = memo(() => {
             authorization: getToken(),
         },
         onChange(info) {
+            console.log(info)
             if (info.file.response.code === 200) {
                 message.success(`${info.file.name} 上传成功`);
             } else {
-                message.error(`文件上传失败: ${info.response.message}`);
+                message.error(`文件上传失败: ${info.file.response.message}`);
             }
         },
     };
 
     return (
         <div className="p-4 flex flex-col items-center">
-            <Avatar size={80} style={{backgroundColor:'#f56a00'}}
-                    shape={"square"}
-                    src={location.state?.groupInfo?.avatar ? location.state.groupInfo.avatar : ''}>
-                G
-            </Avatar>
-            <Upload {...props}>
-                <Button icon={<UploadOutlined />}>上传头像</Button>
-            </Upload>
+            <Flex justify={"center"} vertical>
+                <Upload {...props} className=' cursor-pointer'>
+                    <Avatar size={80} style={{backgroundColor:'#f56a00'}}
+                            shape={"square"}
+                            src={location.state?.groupInfo?.avatar ? location.state.groupInfo.avatar : ''}>
+                        G
+                    </Avatar>
+                </Upload>
+                <Input placeholder="请输入群组名称"/>
+                <Button type={"primary"}>
+                    保存
+                </Button>
+            </Flex>
         </div>
     )
 })
